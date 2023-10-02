720f9eceb38aa2ac16ff93dac2969ff851e97c21
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-02 10:48:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6507059117:AAF-i_j3T05kpbuLff9cdAB7sJtPXH5Pn5w")
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1001596651023"))
-    MONGODB_URL = os.environ.get("MONGODB_URL", "")
+    MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://MrsFallenBot:MrsFallenBot@cluster0.hsedwn2.mongodb.net/?retryWrites=true&w=majority")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "6198858059"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
